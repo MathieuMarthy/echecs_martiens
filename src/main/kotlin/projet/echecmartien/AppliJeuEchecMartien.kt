342bb9e9d2fc94193ab978dffd7647595932bb25
E216120N <fabien.nicou@etu.univ-nantes.fr>
2022-06-15 00:11:04 +0200

    1.2.2

--- src/main/kotlin/projet/echecmartien/AppliJeuEchecMartien.kt
+++ src/main/kotlin/projet/echecmartien/AppliJeuEchecMartien.kt
@@ -1,6 +1,7 @@
 package projet.echecmartien
 
 import javafx.application.Application
+import javafx.event.EventHandler
 import javafx.scene.Scene
 import javafx.scene.input.KeyCombination
 import javafx.stage.Screen
@@ -9,6 +10,7 @@ import javafx.stage.Stage
 import projet.echecmartien.Vue.Charger
 import projet.echecmartien.Vue.GrilleJeu
 import projet.echecmartien.Vue.MainVue
+import projet.echecmartien.Vue.NombreJoueurs
 
 class AppliJeuEchecMartien: Application() {
     override fun start(primaryStage: Stage) {
@@ -17,20 +19,39 @@ class AppliJeuEchecMartien: Application() {
         val vue = MainVue()
         val grille = GrilleJeu()
         val charger =  Charger()
+        var nombreJoueurs = NombreJoueurs()
 
         //Récupération des dimensions de l'écran & mise en plein écran de la scène
         val largeur = Screen.getPrimary().bounds.width
         val longueur = Screen.getPrimary().bounds.height
-        val scene = Scene(charger, largeur, longueur)
         primaryStage.isFullScreen = true
 
+        //Création des scènes
+        val sceneMenu = Scene(grille, largeur, longueur)
+
         //Ajout du style
-        scene.stylesheets.add(AppliJeuEchecMartien::class.java.getResource("style/style.css").toExternalForm())
+        sceneMenu.stylesheets.add(AppliJeuEchecMartien::class.java.getResource("style/style.css").toExternalForm())
+        nombreJoueurs.addStyle()
+        vue.addStyle()
         charger.addStyle()
+
+        //Controleurs
+
+        //NouvellePartie
+        vue.boutonNew.onAction = EventHandler{ primaryStage.scene.root = nombreJoueurs}
+        //Retour (Nouvelle partie)
+        nombreJoueurs.retour.onAction = EventHandler{ primaryStage.scene.root = vue }
+
+        //ChargerPartie
+        vue.boutonLoad.onAction = EventHandler{ primaryStage.scene.root = charger }
+        //Retour (Charger partie)
+        charger.nouveau5.onAction = EventHandler{ primaryStage.scene.root = vue }
+
+
 
         //Mise en place de la scène
         primaryStage.title="Echecs Martiens"
-        primaryStage.scene=scene
+        primaryStage.scene=sceneMenu
         primaryStage.show()
     }
 
