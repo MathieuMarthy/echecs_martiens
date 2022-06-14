dd69113c5cd0a67091d4eaeb9cb412f95f810145
E216120N <fabien.nicou@etu.univ-nantes.fr>
2022-06-14 11:13:07 +0200

    vue menu en cours

--- src/main/kotlin/projet/echecmartien/AppliJeuEchecMartien.kt
+++ src/main/kotlin/projet/echecmartien/AppliJeuEchecMartien.kt
@@ -2,6 +2,8 @@ package projet.echecmartien
 
 import javafx.application.Application
 import javafx.scene.Scene
+import javafx.scene.input.KeyCombination
+import javafx.stage.Screen
 
 import javafx.stage.Stage
 import projet.echecmartien.Vue.MainVue
@@ -9,15 +11,23 @@ import projet.echecmartien.Vue.MainVue
 class AppliJeuEchecMartien: Application() {
     override fun start(primaryStage: Stage) {
 
-       var vue = MainVue()
+        //Initialisation des variables
+        var vue = MainVue()
 
+        //Récupération des dimensions de l'écran & mise en plein écran de la scène
+        var largeur = Screen.getPrimary().bounds.width
+        var longueur = Screen.getPrimary().bounds.height
+        val scene = Scene(vue, largeur, longueur)
+        primaryStage.isFullScreen = true
 
-        val scene = Scene(vue, 715.0, 415.0)
+        //Ajout du style
+        scene.stylesheets.add(AppliJeuEchecMartien::class.java.getResource("style/style.css").toExternalForm())
+        vue.addStyle()
+
+        //Mise en place de la scène
         primaryStage.title="Echecs Martiens"
         primaryStage.scene=scene
         primaryStage.show()
-      
-
     }
 
 }
--- src/main/kotlin/projet/echecmartien/Vue/MainVue.kt
+++ src/main/kotlin/projet/echecmartien/Vue/MainVue.kt
@@ -1,28 +1,54 @@
 package projet.echecmartien.Vue
 
+import javafx.geometry.Pos
 import javafx.scene.control.Button
+import javafx.scene.control.Label
 import javafx.scene.image.Image
 import javafx.scene.image.ImageView
+import javafx.scene.layout.AnchorPane
+import javafx.scene.layout.BorderPane
 import javafx.scene.layout.Pane
-import java.io.FileInputStream
+import javafx.scene.paint.Color
+import javafx.scene.text.Font
+import javafx.scene.text.FontPosture
+import javafx.scene.text.FontWeight
+import javafx.stage.Screen
+import projet.echecmartien.AppliJeuEchecMartien
+import java.net.URL
 
-class MainVue: Pane() {
 
-    val path = System.getProperty("user.dir")
+@Suppress("RECEIVER_NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
+class MainVue: AnchorPane() {
 
-    var image = Image(FileInputStream("$path/src/main/kotlin/projet/echecmartien/Vue/357485-Mars-space-universe-artwork-planet-space_art.jpg"))
+    var label1 = Label("ECHECS MARTIENS")
 
-    var imageView = ImageView(image)
-    var button_test = Button("Test")
+    var astro = ImageView(AppliJeuEchecMartien::class.java.getResource("style/astro2.gif").toExternalForm())
+    var dog = ImageView(AppliJeuEchecMartien::class.java.getResource("style/dog.gif").toExternalForm())
+    var planet = ImageView(AppliJeuEchecMartien::class.java.getResource("style/planet.gif").toExternalForm())
 
 
 
     init {
-        imageView.fitHeight = 715.0
-        imageView.fitWidth = 400.0
-        this.children.add(imageView)
-        this.children.add(button_test)
+        label1.font = Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 20.0);
+        label1.alignment = Pos.BASELINE_CENTER
+        label1.textFill = Color.WHITE
+
+        AnchorPane.setLeftAnchor(label1,325.0)
+        AnchorPane.setTopAnchor(label1,150.0)
+
+        AnchorPane.setRightAnchor(planet,500.0)
+        AnchorPane.setTopAnchor(planet,80.0)
+
+        AnchorPane.setBottomAnchor(astro,0.0)
+        AnchorPane.setRightAnchor(astro,0.0)
+        this.children.addAll(astro,label1)
+
+
     }
 
+    fun addStyle(){
+        this.styleClass.add("fond")
+        label1.styleClass.add("titre")
+    }
 
 }
